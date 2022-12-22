d53cecb60c7be88fe2c1f882816206286a0e7ec3
LiftCube <1464173940@qq.com>
2022-12-22 17:14:48 +0100

    some comments to configuration file

--- config.py
+++ config.py
@@ -1,10 +1,14 @@
+# need to be firstly configured if you want to use this script
 proj_path=r'C:\Users\maqly\Desktop\thesis'
 data_dir_path= r'C:\Users\maqly\Dropbox\NTA'
 fig_path=proj_path+'/fig'
 
+# used to plot different records
 markers=['.','^','s','+','*']
 
+# used in the data cleaning process
 str_similarity_thresh=0.87
+
 feature_self_corr_thresh=0.6
 feature_covid_corr_thresh=0.2
 
@@ -12,10 +16,12 @@ most_corr_rec_num=9
 mode='mean'
 
 
-rec_space=0.1 # the difference between two neibouring records amu value
+rec_space=0.1 # the difference between two adjacent records amu value
 amu_window_size=5
 
+# the range used in data acquisition
 range_idx=4
+
 amu_range = {
     0: ('10.0', '51.0'),
     1: ('10.0', '51.0'),
